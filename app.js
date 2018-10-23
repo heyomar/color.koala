846e487cd18b2b6108c2803b92f22a282969499a
Omar Rasool <webdevromar@gmail.com>
2018-10-23 14:57:27 -0500

    adds function to create a file with the colors generated for the user to download

--- app.js
+++ app.js
@@ -9,6 +9,28 @@
 //   )
 // }
 
+
+// Function to download data to a file
+function download(data, filename, type) {
+var file = new Blob([data], {type: type});
+if (window.navigator.msSaveOrOpenBlob) // IE10+
+    window.navigator.msSaveOrOpenBlob(file, filename);
+else { // Others
+    var a = document.createElement("a"),
+    url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function() {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);  
+    }, 0); 
+}
+}
+
+
+
 // // Variables
 var a = document.querySelector('.a');
 var b = document.querySelector('.b');
@@ -17,37 +39,56 @@ var d = document.querySelector('.d');
 var e = document.querySelector('.e');
 
 
+
+
 // Sets the background color to the value returned from the getColor() function
 function setBackground(){
-  var color = randomColor({
-    count: 5,
-    luminosity: 'light'
-  })
-  
-  // Print the color hex values
-  a.textContent = color[0];
-  b.textContent = color[1];
-  c.textContent = color[2];
-  d.textContent = color[3];
-  e.textContent = color[4];
+var color = randomColor({
+  count: 5,
+  luminosity: 'light',
+  format: 'rgb'
+})
 
-  // Set the color of the containers
-  a.style.background = color[0];
-  b.style.background = color[1];
-  c.style.background = color[2];
-  d.style.background = color[3];
-  e.style.background = color[4];
-  
+// Print the color hex values
+a.textContent = color[0];
+b.textContent = color[1];
+c.textContent = color[2];
+d.textContent = color[3];
+e.textContent = color[4];
+
+// Set the color of the containers
+a.style.background = color[0];
+b.style.background = color[1];
+c.style.background = color[2];
+d.style.background = color[3];
+e.style.background = color[4];
+
+
+
+
 };
 
 document.addEventListener("DOMContentLoaded", function(event){
-  setBackground();
+setBackground();
 })
 
 // Listens for spacebar press to change color
 document.body.onkeyup = function(e){
-  if (e.keyCode == 32) {
-    setBackground();
-  }
+if (e.keyCode == 32) {
+  setBackground();
+}
 };
+
+
+var colorData = `
+/* RGB */
+$color1: ${color[0]};
+$color2: ${color[1]};
+$color3: ${color[2]};
+$color4: ${color[3]};
+$color5: ${color[4]};
+`
+
+download(colorData, 'colors.scss', ".scss")
+
 
