9ef035f5e8cb79e15d60a8014ec9f21042f979bb
Omar Rasool <webdevromar@gmail.com>
2018-10-23 12:54:53 -0500

    adds 5 columns for colors, js to add colors to those columns

--- app.js
+++ app.js
@@ -8,15 +8,36 @@ function getColor() {
   )
 }
 
+// Variables
+const colorLabel = document.querySelector('.color-code');
+var a = document.querySelector('.a');
+var b = document.querySelector('.b');
+var c = document.querySelector('.c');
+var d = document.querySelector('.d');
+var e = document.querySelector('.e');
+
+
 // Sets the background color to the value returned from the getColor() function
 function setBackground(){
-  var bgColor = getColor();
-  document.body.style.background = bgColor;
+  var color1 = getColor()
+  var color2 = getColor()
+  var color3 = getColor()
+  var color4 = getColor()
+  var color5 = getColor()
+
+  // colorLabel.textContent = 'test';
+  a.style.background = color1;
+  b.style.background = color2;
+  c.style.background = color3;
+  d.style.background = color4;
+  e.style.background = color5;
+
 };
 
-// Finally listens for spacebar press to change color
+// Listens for spacebar press to change color
 document.body.onkeyup = function(e){
   if (e.keyCode == 32) {
     setBackground();
   }
 };
+
