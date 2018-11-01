3d0e8d7fc9ecc91e44c8c11ab7efeab19fef5a9b
Omar Rasool <webdevromar@gmail.com>
2018-11-01 14:44:35 -0500

    adds js file for handling special url strings

---
+++ scripts/app.url.js
@@ -0,0 +1,4 @@
+export default function () {
+  // TODO - add funtion to check for Product hunt ref and display a message at top
+  // TODO - add feature that lets you copy the url of the colors display to show later
+}
