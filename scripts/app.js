b3dfdc71f231507bd1b3b62baf2bde6674774c51
Omar Rasool <webdevromar@gmail.com>
2018-12-11 20:38:17 -0600

    lots of changes im to lazy to commit one by one

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,12 @@ import downloadColors from "./app.download";
 import emailColors from "./app.email";
 import helpers from "./app.helpers";
 import meta from "./app.meta";
+import data from "./app.data";
 
 // Run it!
 // ------------------------------
 genColors();
+data();
 // copyToClipBoard();
 // downloadColors();
 // emailColors();
