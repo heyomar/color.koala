dd697e3b606b25f129d093dd37547686b611acb8
Omar Rasool <webdevromar@gmail.com>
2018-12-13 16:04:11 -0600

    cleaning up some js in colors, enable generate button

--- scripts/app.colors.js
+++ scripts/app.colors.js
@@ -1,55 +1,49 @@
-var randomColor = require("randomcolor");
-var convert = require("color-convert");
-
-import * as v from "./app.variables";
+var randomColor = require("randomcolor")
+var convert = require("color-convert")
+import * as v from "./app.variables"
 
 export default () => {
   // Variables for color history
   const colorHistorySection = document.querySelector('.color-history')
-  let paletteContainer = document.createElement("div")
-  let count = 0
+  const paletteContainer = document.createElement("div")
+
   // Variables for randomColor options
-  let luminosity = "";
-  let hue = "";
+  let luminosity = ""
+  let hue = ""
   
   // Main event listener for selection boxes
   document.addEventListener("change", e => {
     if (e.target.classList.contains("colorSelectBox")) {
-      luminosity = e.target.options[e.target.selectedIndex].value;
+      luminosity = e.target.options[e.target.selectedIndex].value
     } else if (e.target.classList.contains("hueSelectBox")) {
-      hue = e.target.options[e.target.selectedIndex].value;
+      hue = e.target.options[e.target.selectedIndex].value
     }
-    setBackground();
-  });
-  // document.querySelector(".generate-button").addEventListener("click", e => {
-  //   e.preventDefault();
-  //   setBackground();
-  // });
+    setColors()
+  })
+
+  document.querySelector(".generate").addEventListener("click", e => {
+    e.preventDefault()
+    setColors()
+  })
+
   // Run randomColor function and set colors
-  function setBackground() {
+  function setColors() {
     const color = randomColor({
       count: 5,
       luminosity: luminosity,
       hue: hue
-    });
+    })
     // Set colors, convert hex to rgb,add the box shadow
-    for (let i = 0; i <= 4; i += 1) {
-      const hex = convert.hex.rgb(color[i]);
-      v.swatches[i].textContent = color[i];
-      v.swatches[i].style.background = color[i];
-    }
+    v.swatches.forEach((e,i) => {
+      e.textContent = color[i]
+      e.style.background = color[i]
+    })
   }
+
   // Listens for spacebar press to change color
-  document.body.onkeydown = e => {
-    if (e.keyCode === 32) {
-      e.preventDefault();
-    }
-  };
-  document.body.onkeyup = e => {
-    if (e.keyCode === 32) {
-      setBackground();
-    }
-  };
+  document.body.onkeydown = e => {if (e.keyCode === 32) {e.preventDefault()}}
+  document.body.onkeyup = e => {if (e.keyCode === 32) {setColors()}}
+
   // COLOR HISTORY //
   function addToHistoryPalettes() {
     v.swatches.forEach(element => {
@@ -72,6 +66,6 @@ export default () => {
   }
 
   document.addEventListener("DOMContentLoaded", () => {
-    setBackground();
-  });
+    setColors()
+  })
 }
