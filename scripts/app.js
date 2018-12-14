bb33cff908d4e097ca1e78a8485b4b468b0d1e40
Omar Rasool <webdevromar@gmail.com>
2018-12-13 19:17:48 -0600

    remove meta js which held maker widget js

--- scripts/app.js
+++ scripts/app.js
@@ -5,15 +5,13 @@ import copyToClipBoard from "./app.copy";
 import downloadColors from "./app.download";
 import emailColors from "./app.email";
 import helpers from "./app.helpers";
-import meta from "./app.meta";
 import data from "./app.data";
 
 // Run it!
 // ------------------------------
 genColors();
 data();
-// copyToClipBoard();
+copyToClipBoard();
 downloadColors();
 // emailColors();
 // helpers();
-// meta();
