e8276670fac0256180af4641eb69484fa96aecbb
Omar Rasool <webdevromar@gmail.com>
2018-12-13 20:47:06 -0600

    refactored email colors js, added helper lib

--- scripts/app.email.js
+++ scripts/app.email.js
@@ -1,14 +1,20 @@
-import * as v from "./app.variables";
+var mailtoLink = require('mailto-link')
+import * as v from "./app.variables"
 
 export default function emailColors() {
-  const emailColorsButton = document.querySelector(".email-button");
-  emailColorsButton.addEventListener("click", () => {
+  const emailButton = document.querySelector(".email")
+  emailButton.addEventListener("click", (e) => {
     const hexColors = [];
-    for (let i = 0; i <= 3; i += 1) {
-      const swatchHexColor = v.swatches[i].textContent;
-      hexColors.push(swatchHexColor);
-    }
-    emailColorsButton.href = `mailto:?subject=Gorgeous%20Color%20Palette%20from%20Color%20Koala&body=Color%20Palette%0D%0A-----------------%0D%0A${hexColors[0]}%0D%0A${hexColors[1]}%0D%0A${hexColors[2]}%0D%0A${hexColors[3]}%0D%0A%0D%0AKoala-ty%20colors%20courtesy%20of%20Color%20Koala!
-  `;
+    v.swatches.forEach((e,i)=> {
+      hexColors.push(v.swatches[i].textContent);
+    });
+
+    emailButton.href = mailtoLink({to: '', subject: 'Gorgeous Palette from Color Koala', body:`
+    My Palette
+    ---------------
+    ${hexColors.map(val => val.toUpperCase()).join("\n")}
+
+    by colorkoala.xyz
+    `})
   });
 }
--- scripts/app.js
+++ scripts/app.js
@@ -12,4 +12,4 @@ genColors();
 data();
 copyToClipBoard();
 downloadColors();
-// emailColors();
+emailColors();
