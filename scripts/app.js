ed38fba52274ab94f052021748a0e1abfe92d510
Omar Rasool <webdevromar@gmail.com>
2018-12-20 14:54:16 -0600

    remove netlify-lambda lib

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,7 @@ import genColors from './app.colors'
 import copyToClipBoard from './app.copy'
 import downloadColors from './app.download'
 import emailColors from './app.email'
+import data from './app.data'
 
 // Run it!
 // ------------------------------
