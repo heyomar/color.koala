3ef719fcbc6f1a89441ea53991000d3285ee3dd7
heyomar <25757036+heyomar@users.noreply.github.com>
2022-02-28 17:20:09 -0600

    formatting

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 // Import JS
 // ------------------------------
-import dotenv from "dotenv"
+import dotenv from 'dotenv'
 dotenv.config()
-import colors from "./app.colors.js"
-import copy from "./app.copy.js"
-import download from "./app.download.js"
-import email from "./app.email.js"
+import colors from './app.colors.js'
+import copy from './app.copy.js'
+import download from './app.download.js'
+import email from './app.email.js'
 
 // Run it!
 // ------------------------------
