7458343cefad040c7924eb9bf2442392466a2612
heyomar <25757036+heyomar@users.noreply.github.com>
2022-02-28 16:59:17 -0600

    pretteir formatting for js

--- scripts/app.colors.js
+++ scripts/app.colors.js
@@ -1,88 +1,90 @@
-import randomColor from 'randomcolor';
-import Airtable from 'airtable';
+import randomColor from "randomcolor"
+import Airtable from "airtable"
 
 export default () => {
-    const base = new Airtable({ apiKey: process.env.API_KEY }).base('app7MHrC2ID2zPNxw');
-    const swatches = document.querySelectorAll('.swatch');
-    // ||----------------[FUNCTIONS]----------------||
-    let hue = '';
-    let luminosity = '';
-    document.addEventListener('change', (e) => {
-        if (e.target.classList.contains('colorSelectBox')) {
-            luminosity = e.target.options[e.target.selectedIndex].value;
-        } else if (e.target.classList.contains('hueSelectBox')) {
-            hue = e.target.options[e.target.selectedIndex].value;
-        }
-        setColors();
-    });
-
-    document.querySelector('.generate').addEventListener('click', (e) => {
-        e.preventDefault();
-        setColors();
-    });
-
-    function setColors() {
-        const color = randomColor({
-            count: 5,
-            luminosity: luminosity,
-            hue: hue,
-        });
-
-        const columns = document.createElement('div');
-        columns.className = 'flex flex-row';
-
-        swatches.forEach((e, i) => {
-            e.textContent = color[i];
-            e.style.background = color[i];
-
-            const colorHistory = document.querySelector('.color-history');
-            const colorBox = document.createElement('div');
-            const hexValue = document.createTextNode(e.textContent);
-
-            colorBox.appendChild(hexValue);
-            colorBox.className = 'swatch-block';
-            colorBox.id = `color${i + 6}`;
-            colorBox.style.backgroundColor = e.textContent;
-
-            colorHistory.appendChild(colorBox);
-            colorBox.setAttribute('data-clipboard-target', `#color${i + 6}`);
-        });
-
-        const count = document.querySelector('.count');
-
-        let paletteFromServer;
-        base('Koala').find('recccR0o11Qm99hrl', (err, record) => {
-            if (err) {
-                console.error(err);
-            }
-            paletteFromServer = record.fields.palettes + 1;
-
-            base('Koala').update(
-                'recccR0o11Qm99hrl',
-                {
-                    palettes: paletteFromServer,
-                },
-                function (err, record) {
-                    if (err) {
-                        console.error(err);
-                    }
-                    count.textContent = record.fields.palettes.toLocaleString();
-                }
-            );
-        });
+  const base = new Airtable({ apiKey: process.env.API_KEY }).base(
+    "app7MHrC2ID2zPNxw"
+  )
+  const swatches = document.querySelectorAll(".swatch")
+  //----------------[FUNCTIONS]----------------//
+  let hue = ""
+  let luminosity = ""
+  document.addEventListener("change", (e) => {
+    if (e.target.classList.contains("colorSelectBox")) {
+      luminosity = e.target.options[e.target.selectedIndex].value
+    } else if (e.target.classList.contains("hueSelectBox")) {
+      hue = e.target.options[e.target.selectedIndex].value
     }
+    setColors()
+  })
 
-    // ||----------------[EVENT LISTENERS]----------------||
-    window.onkeydown = (e) => {
-        if (e.keyCode === 32) {
-            e.preventDefault();
-        }
-    };
-    window.onkeyup = (e) => {
-        if (e.keyCode === 32) {
-            setColors();
-        }
-    };
+  document.querySelector(".generate").addEventListener("click", (e) => {
+    e.preventDefault()
+    setColors()
+  })
 
-    document.addEventListener('DOMContentLoaded', setColors);
-};
+  function setColors() {
+    const color = randomColor({
+      count: 5,
+      luminosity: luminosity,
+      hue: hue,
+    })
+
+    const columns = document.createElement("div")
+    columns.className = "flex flex-row"
+
+    swatches.forEach((e, i) => {
+      e.textContent = color[i]
+      e.style.background = color[i]
+
+      const colorHistory = document.querySelector(".color-history")
+      const colorBox = document.createElement("div")
+      const hexValue = document.createTextNode(e.textContent)
+
+      colorBox.appendChild(hexValue)
+      colorBox.className = "swatch-block"
+      colorBox.id = `color${i + 6}`
+      colorBox.style.backgroundColor = e.textContent
+
+      colorHistory.appendChild(colorBox)
+      colorBox.setAttribute("data-clipboard-target", `#color${i + 6}`)
+    })
+
+    const count = document.querySelector(".count")
+
+    let paletteFromServer
+    base("Koala").find("recccR0o11Qm99hrl", (err, record) => {
+      if (err) {
+        console.error(err)
+      }
+      paletteFromServer = record.fields.palettes + 1
+
+      base("Koala").update(
+        "recccR0o11Qm99hrl",
+        {
+          palettes: paletteFromServer,
+        },
+        function (err, record) {
+          if (err) {
+            console.error(err)
+          }
+          count.textContent = record.fields.palettes.toLocaleString()
+        }
+      )
+    })
+  }
+
+  //----------------[EVENT LISTENERS]----------------//
+  window.onkeydown = (e) => {
+    if (e.keyCode === 32) {
+      e.preventDefault()
+    }
+  }
+  window.onkeyup = (e) => {
+    if (e.keyCode === 32) {
+      setColors()
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", setColors)
+}
--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,15 @@
 // Import JS
 // ------------------------------
-import dotenv from 'dotenv';
-dotenv.config();
-import colors from './app.colors.js';
-import copy from './app.copy.js';
-import download from './app.download.js';
-import email from './app.email.js';
+import dotenv from "dotenv"
+dotenv.config()
+import colors from "./app.colors.js"
+import copy from "./app.copy.js"
+import download from "./app.download.js"
+import email from "./app.email.js"
 
 // Run it!
 // ------------------------------
-colors();
-copy();
-download();
-email();
+colors()
+copy()
+download()
+email()
