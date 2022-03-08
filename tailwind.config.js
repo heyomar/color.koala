ae98117161a479875dd97dd4fb5d2a0e9e4ff57b
heyomar <25757036+heyomar@users.noreply.github.com>
2022-03-07 21:21:27 -0600

    simplify intro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        rubik: "Rubik, sans-serif",
-      },
-      colors: {
-        pink: "#f794ab",
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/forms")],
+	content: ['./index.html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				rubik: 'Rubik, sans-serif',
+			},
+			colors: {
+				pink: '#f794ab',
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms')],
 }
