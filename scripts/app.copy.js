8d99c887f50e3e5374b748c9d547c75f54457fe0
Omar R <heyomar@duck.com>
2023-09-23 07:23:57 -0600

    refactor clipboard code, add comments

--- scripts/app.copy.js
+++ scripts/app.copy.js
@@ -1,21 +1,39 @@
-import ClipboardJS from 'clipboard'
+import ClipboardJS from "clipboard";
 
 export default () => {
-	const clipboard = new ClipboardJS('.copyable')
-	clipboard.on('success', function (e) {
-		e.trigger.firstElementChild.innerHTML = 'Copied!'
-		setTimeout(() => {
-			e.trigger.firstElementChild.innerHTML = e.text
-		}, 450)
-		e.clearSelection()
-	})
+  // Handles the behavior when text is successfully copied.
+  const handleClipboardSuccess = (clipboardEvent, elementAccessor) => {
+    // Get the original text that was copied.
+    const originalText = clipboardEvent.text;
 
-	const historyClipboard = new ClipboardJS('.swatch-block')
-	historyClipboard.on('success', function (e) {
-		e.trigger.innerHTML = 'Copied!'
-		setTimeout(() => {
-			e.trigger.innerHTML = e.text
-		}, 450)
-		e.clearSelection()
-	})
-}
+    // Update the target element's content to 'Copied!'.
+    elementAccessor(clipboardEvent).innerHTML = "Copied!";
+
+    // Revert the content back to the original copied text after 450ms.
+    setTimeout(() => {
+      elementAccessor(clipboardEvent).innerHTML = originalText;
+    }, 450);
+
+    // Clear any active text selection.
+    clipboardEvent.clearSelection();
+  };
+
+  // Accessor for elements with class '.copyable'.
+  const copyableElementAccessor = (clipboardEvent) =>
+    clipboardEvent.trigger.firstElementChild;
+
+  // Accessor for elements with class '.swatch-block'.
+  const swatchBlockAccessor = (clipboardEvent) => clipboardEvent.trigger;
+
+  // Initialize clipboard for '.copyable' elements.
+  const copyableClipboard = new ClipboardJS(".copyable");
+  copyableClipboard.on("success", (clipboardEvent) =>
+    handleClipboardSuccess(clipboardEvent, copyableElementAccessor)
+  );
+
+  // Initialize clipboard for '.swatch-block' elements.
+  const swatchBlockClipboard = new ClipboardJS(".swatch-block");
+  swatchBlockClipboard.on("success", (clipboardEvent) =>
+    handleClipboardSuccess(clipboardEvent, swatchBlockAccessor)
+  );
+};
